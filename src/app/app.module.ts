49b3d16e4bab85be12b2d26c02d8a38d74a8b8f2
Dario Astorga <dastorga@ignetworks.com>
2019-12-23 15:20:39 -0300

    up changes in app-persona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonasComponent } from './personas/personas.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent, PersonasComponent
+    AppComponent, PersonasComponent, PersonaComponent
   ],
   imports: [
     BrowserModule,
