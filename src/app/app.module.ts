7ac4f3ae08f5598cc036561dd479fbf5c8117bca
Dario Astorga <dastorga@ignetworks.com>
2019-12-23 12:15:44 -0300

    add PersonasComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonasComponent } from './personas/personas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PersonasComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/personas/personas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-personas',
+  templateUrl: './personas.component.html',
+  styleUrls: ['./personas.component.css']
+})
+
+export class PersonasComponent {
+
+}
